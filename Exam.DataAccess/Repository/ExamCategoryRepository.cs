efacefd2ad094c9bfb1a5484681c1688a2750fe9
Murad <muradav@code.edu.az>
2024-02-19 17:52:05 +0400

    feat: add ExamCategory Repository

---
+++ Exam.DataAccess/Repository/ExamCategoryRepository.cs
@@ -0,0 +1,60 @@
+﻿using Exam.DataAccess.Data;
+using Exam.DataAccess.Repository.IRepository;
+using Exam.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Exam.DataAccess.Repository
+{
+    public class ExamCategoryRepository : IExamCategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ExamCategoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task Create(ExamCategory entity)
+        {
+            await _db.AddAsync(entity);
+        }
+
+        public async Task<List<ExamCategory>> GetAll(Expression<Func<ExamCategory, bool>> filter = null)
+        {
+            IQueryable<ExamCategory> query = _db.ExamCategories;
+
+            if (query!=null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<ExamCategory> GetOne(Expression<Func<ExamCategory, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<ExamCategory> query = _db.ExamCategories;
+
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            if (query != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public bool Remove(ExamCategory entity)
+        {
+            _db.ExamCategories.Remove(entity);
+            return true;
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ Exam.DataAccess/Repository/IRepository/IExamCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Exam.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.DataAccess.Repository.IRepository
+{
+    public interface IExamCategoryRepository
+    {
+        Task<List<ExamCategory>> GetAll(Expression<Func<ExamCategory, bool>> filter = null);
+        Task<ExamCategory> GetOne(Expression<Func<ExamCategory, bool>> filter = null, bool tracked = true);
+        Task Create(ExamCategory entity);
+        bool Remove(ExamCategory entity);
+        Task Save();
+    }
+}
