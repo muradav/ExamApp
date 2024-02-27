1a57486385bfd540f1c6159c034bbf927ece36f1
Murad <muradav@code.edu.az>
2024-02-27 14:10:18 +0400

    feat: add Image Service

---
+++ Exam.Business/Services/ImageService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Business.Services
+{
+    public static class ImageService
+    {
+        public static bool IsImage(this IFormFile file)
+        {
+            return file.ContentType.Contains("image/");
+        }
+
+        public static bool ValidSize(this IFormFile file, int size)
+        {
+            return file.Length / 1024 < size;
+        }
+
+        public static string SaveImage(this IFormFile file, IWebHostEnvironment env, string folder)
+        {
+            string filename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+            string path = Path.Combine(env.WebRootPath, folder, filename);
+
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            return filename;
+        }
+
+        public static void DeleteImage(string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+    }
+}
